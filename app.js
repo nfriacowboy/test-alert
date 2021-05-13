fef1cb1a95d083c53bc6304386ef4fad74c74f9d
NFRiacowboy <me@nfriacowboy.com>
2021-05-13 15:39:33 +0100

    add app role

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const port = process.env.PORT || 3000;;
 let appInsights = require('applicationinsights');
 
 appInsights.setup("e506aa3a-10af-4f22-a594-656fda5a89ce")
-.setSendLiveMetrics(true)
-.start();
+.setSendLiveMetrics(true):
+appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = "App1";
+appInsights.start();
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
