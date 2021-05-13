c0dfcc7c8d106462d6708fd923d851e585a725f8
NFRiacowboy <me@nfriacowboy.com>
2021-05-13 15:42:32 +0100

    bug fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;;
 let appInsights = require('applicationinsights');
 
 appInsights.setup("e506aa3a-10af-4f22-a594-656fda5a89ce")
-.setSendLiveMetrics(true):
+.setSendLiveMetrics(true);
 appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = "App1";
 appInsights.start();
 
